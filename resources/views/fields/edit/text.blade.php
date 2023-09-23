50f15bc784a6a6156aacfb158c6c1cdf3ddce81d
taiki-hashibe <tb.ba.700@gmail.com>
2023-09-24 03:10:52 +0900

    create textarea

---
+++ resources/views/fields/edit/text.blade.php
@@ -0,0 +1,9 @@
+<div class="mb-4">
+    <label for="{{ $name }}" class="block text-slate-500 text-sm font-bold  mb-1.5">{{ $label }}</label>
+    <textarea
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-sky-400"
+        name="{{ $name }}" value="{{ old($name, isset($value) ? $value : '') }}"></textarea>
+    @error($name)
+        <span style="color:red">{{ $message }}</span>
+    @enderror
+</div>
